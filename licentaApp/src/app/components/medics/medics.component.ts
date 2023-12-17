d73bccaad833f73468f844ec1ec803f91d2b85bc
PopescuStefan1 <stefan.ro95@gmail.com>
2023-12-17 16:42:59 +0200

    Create cards for medics

--- licentaApp/src/app/components/medics/medics.component.ts
+++ licentaApp/src/app/components/medics/medics.component.ts
@@ -9,6 +9,8 @@ import { DataService } from "src/app/services/data.service";
 })
 export class MedicsComponent implements OnInit {
   medics: Medic[] = [];
+  defaultPhotoUrl: string =
+    "https://images.unsplash.com/photo-1607368386669-d940ce438fba?q=80&w=1843&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   constructor(private dataService: DataService) {}
 
