e9e58243285f17af773f2cbfcf7896eb6c8bf6e6
PopescuStefan1 <stefan.ro95@gmail.com>
2023-12-17 19:20:21 +0200

    Add functionality to go to next image in carousel

--- licentaApp/src/app/components/home/carousel/carousel.component.ts
+++ licentaApp/src/app/components/home/carousel/carousel.component.ts
@@ -12,8 +12,25 @@ export class CarouselComponent implements OnInit {
     "https://images.unsplash.com/photo-1511174511562-5f7f18b874f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1581056771392-8a90ddb76831?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
+  activeImageIndex: number = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onPrevClick() {
+    if (this.activeImageIndex === 0) {
+      this.activeImageIndex = this.carouselImgUrls.length - 1;
+      return;
+    }
+    this.activeImageIndex--;
+  }
+
+  onNextClick() {
+    if (this.activeImageIndex === this.carouselImgUrls.length - 1) {
+      this.activeImageIndex = 0;
+      return;
+    }
+    this.activeImageIndex++;
+  }
 }
