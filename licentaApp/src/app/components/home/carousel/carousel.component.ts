84f8de0ba03beb0c499fdfc696c6666a79f2310c
PopescuStefan1 <stefan.ro95@gmail.com>
2023-12-17 20:00:50 +0200

    Add text & gradient over carousel images

--- licentaApp/src/app/components/home/carousel/carousel.component.ts
+++ licentaApp/src/app/components/home/carousel/carousel.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./carousel.component.css"],
 })
 export class CarouselComponent implements OnInit {
-  carouselImgUrls: string[] = [
-    "https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1598300188480-626f2f79ab8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1511174511562-5f7f18b874f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1581056771392-8a90ddb76831?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  carouselSections: { url: string; text: string }[] = [
+    {
+      url: "https://images.unsplash.com/photo-1581056771392-8a90ddb76831?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Improving life quality and bringing smiles to you and your loved ones - since 2023",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1576091160550-2173dba999ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Integrating technology into medicine for excellence in healthcare",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1598300188480-626f2f79ab8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Fast, reliable medical screening for all of your needs",
+    },
+    {
+      url: "https://images.unsplash.com/photo-1511174511562-5f7f18b874f8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      text: "Cutting edge research facilites to provide world-class care",
+    },
   ];
   activeImageIndex: number = 0;
 
@@ -20,14 +32,14 @@ export class CarouselComponent implements OnInit {
 
   onPrevClick() {
     if (this.activeImageIndex === 0) {
-      this.activeImageIndex = this.carouselImgUrls.length - 1;
+      this.activeImageIndex = this.carouselSections.length - 1;
       return;
     }
     this.activeImageIndex--;
   }
 
   onNextClick() {
-    if (this.activeImageIndex === this.carouselImgUrls.length - 1) {
+    if (this.activeImageIndex === this.carouselSections.length - 1) {
       this.activeImageIndex = 0;
       return;
     }
