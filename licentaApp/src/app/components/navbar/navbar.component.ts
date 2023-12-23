2a58d456319fc7323ce8216341df369a5cbd4379
PopescuStefan1 <stefan.ro95@gmail.com>
2023-12-23 16:11:34 +0200

    Change navbar icons to mat icons

--- licentaApp/src/app/components/navbar/navbar.component.ts
+++ licentaApp/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean = false;
+  private userSub: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
