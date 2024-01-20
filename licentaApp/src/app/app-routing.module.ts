4ce006d35579b464c528ba45715b8e21bbd5876a
PopescuStefan1 <stefan.ro95@gmail.com>
2024-01-20 19:01:46 +0200

    Add autologin and autologout methods and add profileguard

--- licentaApp/src/app/app-routing.module.ts
+++ licentaApp/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from "./components/home/home.component";
 import { MedicsComponent } from "./components/medics/medics.component";
 import { AuthComponent } from "./components/auth/auth.component";
 import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { ProfileGuard } from "./guards/profile.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: AuthComponent },
   { path: "medics", component: MedicsComponent },
-  { path: "profile/:userId", component: UserProfileComponent },
+  { path: "profile/:userId", component: UserProfileComponent, canActivate: [ProfileGuard] },
   { path: "**", redirectTo: "" },
 ];
 
