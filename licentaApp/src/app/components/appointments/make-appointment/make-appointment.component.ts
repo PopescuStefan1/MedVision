0a864b79f715a01972ffe671ff2b9899904e411a
PopescuStefan1 <stefan.ro95@gmail.com>
2024-02-23 19:05:03 +0200

    Fix form validity

--- licentaApp/src/app/components/appointments/make-appointment/make-appointment.component.ts
+++ licentaApp/src/app/components/appointments/make-appointment/make-appointment.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
 import { MatSelectChange } from "@angular/material/select";
 import { Observable } from "rxjs";
 import { AppointmentService } from "src/app/services/appointment.service";
@@ -10,7 +19,7 @@ import { AppointmentService } from "src/app/services/appointment.service";
   styleUrls: ["./make-appointment.component.css"],
 })
 export class MakeAppointmentComponent implements OnInit {
-  newAppointmentForm!: FormGroup;
+  appointmentForm!: FormGroup;
   initialFormValue: any;
   availableSpecialties$: Observable<string[]> = new Observable();
   cities$: Observable<string[]>;
@@ -21,21 +30,49 @@ export class MakeAppointmentComponent implements OnInit {
 
   ngOnInit(): void {
     this.createAppointmentForm();
+
+    this.cities$.subscribe((cities) => {
+      const cityControl = this.appointmentForm.get("city");
+      cityControl?.setValidators([Validators.required, this.optionsValidator(cities)]);
+      cityControl?.updateValueAndValidity();
+    });
+  }
+
+  optionsValidator(options: any[]): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (Validators.required(control) !== null) {
+        // If required validation fails, return null (indicating the required validation error)
+        return null;
+      }
+
+      const selectedValue = control.value;
+      if (!options.includes(selectedValue)) {
+        return { oneOf: true }; // Custom error key to indicate oneOf validation failure
+      }
+
+      return null;
+    };
   }
 
   createAppointmentForm() {
-    this.newAppointmentForm = this.formBuilder.group({
-      city: new FormControl(["", [Validators.required]]),
+    this.appointmentForm = this.formBuilder.group({
+      city: new FormControl(["", { validators: [], asyncValidators: [Validators.required] }]),
       specialty: new FormControl(["", [Validators.required]]),
     });
 
-    this.initialFormValue = this.newAppointmentForm.value;
+    this.initialFormValue = this.appointmentForm.value;
   }
 
   onCityChange(selection: MatSelectChange) {
     const selectedCity: string = selection.value;
 
     this.availableSpecialties$ = this.appointmentService.getSpecialtiesForCity(selectedCity);
+
+    this.availableSpecialties$.subscribe((specialties) => {
+      const specialtyControl = this.appointmentForm.get("specialty");
+      specialtyControl?.setValidators([Validators.required, this.optionsValidator(specialties)]);
+      specialtyControl?.updateValueAndValidity();
+    });
   }
 
   onSubmit() {}
