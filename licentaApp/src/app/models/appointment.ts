b3820aa88a2f8f44033c4c4eb6b63dac45c264ff
PopescuStefan1 <stefan.ro95@gmail.com>
2024-02-24 17:24:22 +0200

    Reset time field after changing date

--- licentaApp/src/app/models/appointment.ts
+++ licentaApp/src/app/models/appointment.ts
@@ -1,5 +1,11 @@
 export interface Appointment {
   medicId: string;
   patientId: string;
+  city: string;
   startTimestamp: Date;
+  firstName: string;
+  lastName: string;
+  telephone: string;
+  email: string;
+  comment?: string;
 }
