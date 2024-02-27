39506847557d006dce26853d6d34bfd359c8d22e
PopescuStefan1 <stefan.ro95@gmail.com>
2024-02-27 18:37:38 +0200

    Add schedule link to navbar

--- licentaApp/src/app/components/navbar/navbar.component.ts
+++ licentaApp/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { Subscription } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { UserProfile } from "src/app/models/user-profile";
 import { AuthService } from "src/app/services/auth.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "app-navbar",
@@ -12,13 +14,17 @@ export class NavbarComponent implements OnInit, OnDestroy {
   isAuthenticated: boolean = false;
   userId: string | null = null;
   private userSub: Subscription = new Subscription();
+  userData$: Observable<UserProfile> = new Observable();
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.userSub = this.authService.user.subscribe((user) => {
       this.isAuthenticated = !!user;
       this.userId = user ? user.id : null;
+      if (this.userId) {
+        this.userData$ = this.userService.getUserData(this.userId);
+      }
     });
   }
 
