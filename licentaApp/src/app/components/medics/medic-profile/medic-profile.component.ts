11712552cdfd06bf675f41580a51c2df6f961ec8
PopescuStefan1 <stefan.ro95@gmail.com>
2024-03-16 15:09:59 +0200

    Add medic-profile page

---
+++ licentaApp/src/app/components/medics/medic-profile/medic-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-medic-profile",
+  templateUrl: "./medic-profile.component.html",
+  styleUrls: ["./medic-profile.component.css"],
+})
+export class MedicProfileComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
