dc0f8ade091deca8dc39266ad17e977e881f8c92
PopescuStefan1 <stefan.ro95@gmail.com>
2024-03-24 14:40:33 +0200

    Add UI for not-authorized page

--- licentaApp/src/app/components/not-authorized/not-authorized.component.ts
+++ licentaApp/src/app/components/not-authorized/not-authorized.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Observable, Subscription } from "rxjs";
+import { User } from "src/app/models/user.model";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-not-authorized',
-  templateUrl: './not-authorized.component.html',
-  styleUrls: ['./not-authorized.component.css']
+  selector: "app-not-authorized",
+  templateUrl: "./not-authorized.component.html",
+  styleUrls: ["./not-authorized.component.css"],
 })
 export class NotAuthorizedComponent implements OnInit {
+  currentUser: User | null = null;
+  userSub: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.currentUser = user;
+    });
   }
 
+  onLogoutClick(): void {
+    this.authService.logout();
+    this.router.navigate(["/"]);
+  }
+
+  onReturnToMainPageClick(): void {
+    this.router.navigate(["/"]);
+  }
 }
