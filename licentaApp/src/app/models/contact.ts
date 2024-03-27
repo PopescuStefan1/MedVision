840154b0f750caf1f7019c415b3b6731256b9948
PopescuStefan1 <stefan.ro95@gmail.com>
2024-03-27 21:46:39 +0200

    Add functionality for saving contact data to DB

---
+++ licentaApp/src/app/models/contact.ts
@@ -0,0 +1,14 @@
+export interface Contact {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  message: string;
+  medicProblem: boolean;
+  appointmentProblem: boolean;
+  scheduleProblem: boolean;
+  accountProblem: boolean;
+  otherProblem: boolean;
+  medicJoin: boolean;
+  otherReason: boolean;
+}
