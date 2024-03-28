9304fe251b6984a679d9fa886b2c154b3c91836e
PopescuStefan1 <stefan.ro95@gmail.com>
2024-03-28 21:04:06 +0200

    Add button to scroll about page

--- licentaApp/src/app/components/about/about.component.ts
+++ licentaApp/src/app/components/about/about.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  constructor(private elementRef: ElementRef) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onArrowClick(): void {
+    const element = this.elementRef.nativeElement.querySelector("#vision");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
   }
-
 }
