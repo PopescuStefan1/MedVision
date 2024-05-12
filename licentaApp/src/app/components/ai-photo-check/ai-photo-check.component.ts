4a7eb0cba43496781de0432ba27805a547a662da
PopescuStefan1 <stefan.ro95@gmail.com>
2024-05-12 16:31:07 +0300

    Add model to schedule page

--- licentaApp/src/app/components/ai-photo-check/ai-photo-check.component.ts
+++ licentaApp/src/app/components/ai-photo-check/ai-photo-check.component.ts
@@ -1,174 +1,37 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { DescriptionDialogComponent } from "./description-dialog/description-dialog.component";
 import { HelpDialogComponent } from "./help-dialog/help-dialog.component";
-import * as tf from "@tensorflow/tfjs";
+import { LesionClassificationService } from "src/app/services/lesion-classification.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-ai-photo-check",
   templateUrl: "./ai-photo-check.component.html",
   styleUrls: ["./ai-photo-check.component.css"],
 })
-export class AIPhotoCheckComponent implements OnInit {
+export class AIPhotoCheckComponent implements OnInit, OnDestroy {
   uploadedFile?: File;
   imageUrl: string | ArrayBuffer | null = null;
   predictionMade: boolean = false;
+  modelLoaded: boolean = false;
+  predictionStatusSubscription: Subscription = new Subscription();
 
-  mockResults = [
-    {
-      name: "Actinic keratosis and intraepithelial carcinoma / Bowen's disease",
-      shortName: "akiec",
-      value: 0,
-      description:
-        "Actinic Keratoses (AKs), also known as Solar Keratoses, are precancerous skin lesions that typically develop on areas of the skin that have been exposed to the sun over time. If left untreated, some AKs may progress to intraepithelial Carcinoma, also known as Bowen's disease.",
-      needsConsult: true,
-    },
-    {
-      name: "Basal cell carcinoma",
-      shortName: "bcc",
-      value: 0,
-      description:
-        "Basal cell carcinoma (BCC) is the most common type of skin cancer, accounting for about 80% of all skin cancer cases. It typically develops in areas of the skin that have been exposed to the sun, such as the face, neck, ears, scalp, shoulders, and back. BCC arises from the basal cells, which are found in the deepest layer of the epidermis, the outermost layer of the skin.",
-      needsConsult: true,
-    },
-    {
-      name: "Benign keratosis-like lesion",
-      shortName: "bkl",
-      value: 0.001,
-      description:
-        "Benign keratosis-like lesions, also known as seborrheic keratoses, are common non-cancerous growths that appear on the skin, especially in older adults. They are usually painless and do not pose any health risks.",
-      needsConsult: false,
-    },
-    {
-      name: "Dermatofibroma",
-      shortName: "df",
-      value: 0,
-      description:
-        "Dermatofibroma is a common benign skin lesion, often referred to as a fibrous histiocytoma. They are usually harmless and don't require treatment unless they become symptomatic or cosmetically bothersome.",
-      needsConsult: false,
-    },
-    {
-      name: "Melanoma",
-      shortName: "mel",
-      value: 0.003,
-      description:
-        "Melanoma is a type of skin cancer that originates in melanocytes, the cells responsible for producing melanin, the pigment that gives skin its color. Melanoma is a potentially serious form of skin cancer that requires prompt diagnosis and treatment for the best outcome.",
-      needsConsult: true,
-    },
-    {
-      name: "Melanocytic nevus",
-      shortName: "nv",
-      value: 0.996,
-      description:
-        "Melanocytic nevi, commonly known as moles, are benign growths on the skin that develop when melanocytes (pigment-producing cells) grow in clusters. Although most moles are benign, it's essential to monitor them for any changes that could indicate skin cancer, such as melanoma.",
-      needsConsult: false,
-    },
-    {
-      name: "Vascular lesion",
-      shortName: "vasc",
-      value: 0,
-      description:
-        "Vascular lesions are abnormalities in the blood vessels that can occur anywhere in the body. These lesions can vary in size, shape, and severity, and they may present as visible abnormalities on the skin or within deeper tissues.",
-      needsConsult: true,
-    },
-  ];
-  realResults = [
-    {
-      name: "Actinic keratosis and intraepithelial carcinoma / Bowen's disease",
-      shortName: "akiec",
-      value: 0,
-      description:
-        "Actinic Keratoses (AKs), also known as Solar Keratoses, are precancerous skin lesions that typically develop on areas of the skin that have been exposed to the sun over time. If left untreated, some AKs may progress to intraepithelial Carcinoma, also known as Bowen's disease.",
-      needsConsult: true,
-    },
-    {
-      name: "Basal cell carcinoma",
-      shortName: "bcc",
-      value: 0,
-      description:
-        "Basal cell carcinoma (BCC) is the most common type of skin cancer, accounting for about 80% of all skin cancer cases. It typically develops in areas of the skin that have been exposed to the sun, such as the face, neck, ears, scalp, shoulders, and back. BCC arises from the basal cells, which are found in the deepest layer of the epidermis, the outermost layer of the skin.",
-      needsConsult: true,
-    },
-    {
-      name: "Benign keratosis-like lesion",
-      shortName: "bkl",
-      value: 0,
-      description:
-        "Benign keratosis-like lesions, also known as seborrheic keratoses, are common non-cancerous growths that appear on the skin, especially in older adults. They are usually painless and do not pose any health risks.",
-      needsConsult: false,
-    },
-    {
-      name: "Dermatofibroma",
-      shortName: "df",
-      value: 0,
-      description:
-        "Dermatofibroma is a common benign skin lesion, often referred to as a fibrous histiocytoma. They are usually harmless and don't require treatment unless they become symptomatic or cosmetically bothersome.",
-      needsConsult: false,
-    },
-    {
-      name: "Melanoma",
-      shortName: "mel",
-      value: 0,
-      description:
-        "Melanoma is a type of skin cancer that originates in melanocytes, the cells responsible for producing melanin, the pigment that gives skin its color. Melanoma is a potentially serious form of skin cancer that requires prompt diagnosis and treatment for the best outcome.",
-      needsConsult: true,
-    },
-    {
-      name: "Melanocytic nevus",
-      shortName: "nv",
-      value: 0,
-      description:
-        "Melanocytic nevi, commonly known as moles, are benign growths on the skin that develop when melanocytes (pigment-producing cells) grow in clusters. Although most moles are benign, it's essential to monitor them for any changes that could indicate skin cancer, such as melanoma.",
-      needsConsult: false,
-    },
-    {
-      name: "Vascular lesion",
-      shortName: "vasc",
-      value: 0,
-      description:
-        "Vascular lesions are abnormalities in the blood vessels that can occur anywhere in the body. These lesions can vary in size, shape, and severity, and they may present as visible abnormalities on the skin or within deeper tissues.",
-      needsConsult: true,
-    },
-  ];
-
-  linearModel!: tf.Sequential;
-  prediction: any;
-
-  model!: tf.GraphModel;
-  predictions: any;
-
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private lesionClassificationService: LesionClassificationService) {}
 
   ngOnInit(): void {
-    this.loadModel();
-  }
-
-  async loadModel() {
-    this.model = await tf.loadGraphModel("/assets/model.json");
-  }
-
-  predict(image: HTMLImageElement) {
-    const pred = tf.tidy(() => {
-      let img = tf.browser.fromPixels(image);
-      img = tf.image.resizeBilinear(img, [32, 32]); // Resize the image to match model input shape
-      img = img.div(255); // Normalize pixel values between 0 and 1
-      img = img.reshape([1, 32, 32, 3]);
-      img = tf.cast(img, "float32");
-
-      const output = this.model.predict(img) as any;
-
-      this.predictions = Array.from(output.dataSync());
+    this.predictionStatusSubscription = this.lesionClassificationService.predictionStatus$.subscribe(
+      (predictionStatus) => {
+        this.predictionMade = predictionStatus;
+      }
+    );
+    this.lesionClassificationService.loadModel().then(() => {
+      this.modelLoaded = true;
     });
+  }
 
-    console.log(this.predictions);
-    for (let i = 0; i < this.realResults.length; i++) {
-      console.log(this.realResults[i].name);
-      this.realResults[i].value = this.predictions[i];
-    }
-
-    console.log(this.realResults);
-
-    this.predictionMade = true;
+  ngOnDestroy(): void {
+    this.predictionStatusSubscription.unsubscribe();
   }
 
   onFileChange(event: any): void {
@@ -184,15 +47,13 @@ export class AIPhotoCheckComponent implements OnInit {
   }
 
   private handleFile(file?: File): void {
-    this.predictionMade = false;
-
     const reader = new FileReader();
     const img = new Image();
 
     reader.onload = (e) => {
       this.imageUrl = reader.result;
       img.onload = () => {
-        this.predict(img);
+        this.lesionClassificationService.predict(img);
       };
       img.src = reader.result as string;
     };
@@ -202,7 +63,6 @@ export class AIPhotoCheckComponent implements OnInit {
     }
   }
 
-  // Mock method, change later
   getTopResults(): {
     name: string;
     shortName: string;
@@ -210,7 +70,7 @@ export class AIPhotoCheckComponent implements OnInit {
     description: string;
     needsConsult: boolean;
   }[] {
-    const sortedResults = [...this.realResults].sort((a, b) => b.value - a.value);
+    const sortedResults = [...this.lesionClassificationService.realResults].sort((a, b) => b.value - a.value);
 
     return sortedResults.slice(0, 3);
   }
--- licentaApp/src/app/components/schedule/schedule-details/schedule-details.component.ts
+++ licentaApp/src/app/components/schedule/schedule-details/schedule-details.component.ts
@@ -1,7 +1,9 @@
 import { DatePipe } from "@angular/common";
-import { Component, Inject, OnInit } from "@angular/core";
+import { Component, ElementRef, Inject, OnDestroy, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Subscription } from "rxjs";
 import { Appointment } from "src/app/models/appointment";
+import { LesionClassificationService } from "src/app/services/lesion-classification.service";
 
 interface DialogData {
   appointment: Appointment;
@@ -13,70 +15,33 @@ interface DialogData {
   templateUrl: "./schedule-details.component.html",
   styleUrls: ["./schedule-details.component.css"],
 })
-export class ScheduleDetailsComponent implements OnInit {
-  mockResults = [
-    {
-      name: "Actinic keratosis and intraepithelial carcinoma / Bowen's disease",
-      shortName: "akiec",
-      value: 0,
-      description:
-        "Actinic Keratoses (AKs), also known as Solar Keratoses, are precancerous skin lesions that typically develop on areas of the skin that have been exposed to the sun over time. If left untreated, some AKs may progress to intraepithelial Carcinoma, also known as Bowen's disease.",
-      needsConsult: true,
-    },
-    {
-      name: "Basal cell carcinoma",
-      shortName: "bcc",
-      value: 0,
-      description:
-        "Basal cell carcinoma (BCC) is the most common type of skin cancer, accounting for about 80% of all skin cancer cases. It typically develops in areas of the skin that have been exposed to the sun, such as the face, neck, ears, scalp, shoulders, and back. BCC arises from the basal cells, which are found in the deepest layer of the epidermis, the outermost layer of the skin.",
-      needsConsult: true,
-    },
-    {
-      name: "Benign keratosis-like lesion",
-      shortName: "bkl",
-      value: 0.001,
-      description:
-        "Benign keratosis-like lesions, also known as seborrheic keratoses, are common non-cancerous growths that appear on the skin, especially in older adults. They are usually painless and do not pose any health risks.",
-      needsConsult: false,
-    },
-    {
-      name: "Dermatofibroma",
-      shortName: "df",
-      value: 0,
-      description:
-        "Dermatofibroma is a common benign skin lesion, often referred to as a fibrous histiocytoma. They are usually harmless and don't require treatment unless they become symptomatic or cosmetically bothersome.",
-      needsConsult: false,
-    },
-    {
-      name: "Melanoma",
-      shortName: "mel",
-      value: 0.003,
-      description:
-        "Melanoma is a type of skin cancer that originates in melanocytes, the cells responsible for producing melanin, the pigment that gives skin its color. Melanoma is a potentially serious form of skin cancer that requires prompt diagnosis and treatment for the best outcome.",
-      needsConsult: true,
-    },
-    {
-      name: "Melanocytic nevus",
-      shortName: "nv",
-      value: 0.996,
-      description:
-        "Melanocytic nevi, commonly known as moles, are benign growths on the skin that develop when melanocytes (pigment-producing cells) grow in clusters. Although most moles are benign, it's essential to monitor them for any changes that could indicate skin cancer, such as melanoma.",
-      needsConsult: false,
-    },
-    {
-      name: "Vascular lesion",
-      shortName: "vasc",
-      value: 0,
-      description:
-        "Vascular lesions are abnormalities in the blood vessels that can occur anywhere in the body. These lesions can vary in size, shape, and severity, and they may present as visible abnormalities on the skin or within deeper tissues.",
-      needsConsult: true,
-    },
-  ];
+export class ScheduleDetailsComponent implements OnInit, OnDestroy {
   mockAnalysisClicked: boolean = false;
+  predictionStatusSubscription: Subscription = new Subscription();
+  predictionMade: boolean = false;
+  modelLoaded: boolean = false;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, private datePipe: DatePipe) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private datePipe: DatePipe,
+    private lesionClassificationService: LesionClassificationService,
+    private elementRef: ElementRef
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.predictionStatusSubscription = this.lesionClassificationService.predictionStatus$.subscribe(
+      (predictionStatus) => {
+        this.predictionMade = predictionStatus;
+      }
+    );
+    this.lesionClassificationService.loadModel().then(() => {
+      this.modelLoaded = true;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.predictionStatusSubscription.unsubscribe();
+  }
 
   getAppointmentTimesAsString(date: Date) {
     const endTime = new Date(date);
@@ -88,7 +53,6 @@ export class ScheduleDetailsComponent implements OnInit {
     return `${startTimeString} - ${endTimeString}`;
   }
 
-  // Mock method, change later
   getTopResults(): {
     name: string;
     shortName: string;
@@ -96,12 +60,14 @@ export class ScheduleDetailsComponent implements OnInit {
     description: string;
     needsConsult: boolean;
   }[] {
-    const sortedResults = this.mockResults.sort((a, b) => b.value - a.value);
+    const sortedResults = [...this.lesionClassificationService.realResults].sort((a, b) => b.value - a.value);
 
     return sortedResults.slice(0, 3);
   }
 
   onGetAnalysisClick(): void {
-    this.mockAnalysisClicked = true;
+    if (this.data.appointment.imgUrl) {
+      this.lesionClassificationService.predictFromImageUrl(this.data.appointment.imgUrl);
+    }
   }
 }
