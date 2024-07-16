091c0c80ca2827dccdd3e351b195f7cfae281d4b
PopescuStefan1 <stefan.ro95@gmail.com>
2024-07-16 17:01:11 +0300

    Add ReCAPTCHA3 using Firebase AppCheck

---
+++ licentaApp/src/app/firebase-init.ts
@@ -0,0 +1,16 @@
+// // firebase-initialization.ts
+// import {
+//   initializeAppCheck,
+//   ReCaptchaV3Provider,
+// } from '@angular/fire/app-check';
+// import { initializeApp } from 'firebase/app';
+// import { environment } from 'src/environments/environment';
+
+// const app = initializeApp(environment.firebase); // Replace with your Firebase config object
+
+// const appCheck = initializeAppCheck(app, {
+//   provider: new ReCaptchaV3Provider(environment.recaptchaV3SiteKey),
+//   isTokenAutoRefreshEnabled: true, // Optional: Auto-refresh token
+// });
+
+// appCheck.activate();
