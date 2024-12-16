f1a6e1305219a5c66c0bd4c8d72469ecccebb527
PopescuStefan1 <stefan.ro95@gmail.com>
2024-12-16 23:19:21 +0200

    Update gitignore

--- licentaApp/src/environments/environment.prod.ts
+++ licentaApp/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDE8NW8L3l2Rtf0chpZqlUWZVwBPxSm18I',
+    authDomain: 'licenta-fb443.firebaseapp.com',
+    databaseURL:
+      'https://licenta-fb443-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'licenta-fb443',
+    storageBucket: 'licenta-fb443.appspot.com',
+    messagingSenderId: '23841865369',
+    appId: '1:23841865369:web:35ac26aa4ae248a8070208',
+    measurementId: 'G-DL565TRXP3',
+  },
+  recaptchaV3SiteKey: '6Lf7MxEqAAAAAE0vR7jYA6ubCfcRb4f5jTvRCsGX',
 };
