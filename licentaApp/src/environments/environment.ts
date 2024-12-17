6536d9f91f6da7651469111d2875d873279b43a8
PopescuStefan1 <stefan.ro95@gmail.com>
2024-12-17 13:54:39 +0200

    Fix

--- licentaApp/src/environments/environment.ts
+++ licentaApp/src/environments/environment.ts
@@ -5,16 +5,17 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: process.env['FIREBASE_API_KEY'] || '',
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
-    databaseURL: process.env['FIREBASE_DATABASE_URL'] || '',
-    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
-    appId: process.env['FIREBASE_APP_ID'] || '',
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || '',
+    apiKey: 'AIzaSyDE8NW8L3l2Rtf0chpZqlUWZVwBPxSm18I',
+    authDomain: 'licenta-fb443.firebaseapp.com',
+    databaseURL:
+      'https://licenta-fb443-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'licenta-fb443',
+    storageBucket: 'licenta-fb443.appspot.com',
+    messagingSenderId: '23841865369',
+    appId: '1:23841865369:web:35ac26aa4ae248a8070208',
+    measurementId: 'G-DL565TRXP3',
   },
-  recaptchaV3SiteKey: process.env['RECAPTCHA_SITE_KEY'] || '',
+  recaptchaV3SiteKey: '6Lf7MxEqAAAAAE0vR7jYA6ubCfcRb4f5jTvRCsGX',
 };
 
 /*
